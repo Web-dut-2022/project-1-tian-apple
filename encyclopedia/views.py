014ea5969c495e3a99a3fe264e2d97989cd4643b
Jintian <94154682+tian-apple@users.noreply.github.com>
2022-04-08 16:07:00 +0800

    Add files via upload

--- encyclopedia/views.py
+++ encyclopedia/views.py
@@ -1,3 +1,6 @@
+import random
+import markdown
+from django.http import HttpResponse, HttpResponseNotFound
 from django.shortcuts import render
 
 from . import util
@@ -8,3 +11,21 @@ def index(request):
         "entries": util.list_entries()
     })
 
+def search(request):
+    title = request.GET.get("q")
+    Entry =util.get_entry(title)
+    if Entry == None:
+        return HttpResponseNotFound("Not Found")
+    else: return render(request, "encyclopedia/display.html", {
+        "searchname":title,
+        "Entry":markdown.markdown(Entry)
+    })
+
+def random_entry(request):
+    list=util.list_entries();
+    r = random.randint(0,len(list)-1)
+    Entry=util.get_entry(list[r])
+    return render(request, "encyclopedia/display.html", {
+        "searchname":"random",
+        "Entry":markdown.markdown(Entry)
+    })
